97898b6a095bf1106a920d71eaf11eee46a6f00a
willram24 <wramirez23@gmail.com>
2021-02-13 15:53:18 -0800

    javascript updated

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,39 @@
-document.getElementById('generate').addEventListener('click', => () {
+document.getElementById('generate').addEventListener('click', () => {
 
-let hasLength = prompt("How long would you like your password to be? (Between 8 and 128 characters)")
-let hasLowercase= confirm("Would you like your password to contain lowercase letters?")
-let hasUppercase = confirm("Would you like your password to contain uppercase letters?")
-let hasNumbers = confirm("Would you like your password to contain numbers?")
-let hasSymbols = confirm("Would you like your password to contain")
+  let requestedLength = prompt("How long would you like your password to be? (Between 8 and 128 characters)")
+  let hasLowercase = confirm("Would you like your password to contain lowercase letters?")
+  let hasUppercase = confirm("Would you like your password to contain uppercase letters?")
+  let hasNumbers = confirm("Would you like your password to contain numbers?")
+  let hasSymbols = confirm("Would you like your password to contain symbols?")
+
+  let password = ""
+  let lowercase = "abcdefghijklmnopqrstuvwxyz"
+  let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let numbers = "0123456789"
+  let symbols = "!@#$%^&*"
+  let charSet = ""
+
+  if (hasLowercase) {
+    charSet += lowercase
+  }
+  if (hasUppercase) {
+    charSet += uppercase
+  }
+  if (hasNumbers) {
+    charSet += numbers
+  }
+  if (hasSymbols) {
+    charSet += symbols
+  }
+
+  for (let i = 0; i < parseInt(requestedLength); i++) {
+    password += charSet[Math.floor(Math.random() * charSet.length)]
+  }
+  console.log(password)
+
 
 })
+// Put all letters we want in one bucket
+// Randomly pick a type of letter
+// Closing my eyes and grabbing a letter
+// Put it in the generated password
